95957284fd577944248eb233d972a0b8d0d04b60
Rdu San <xiltepin@Servidor>
2024-09-12 14:52:39 +0900

    deleted unneeded api file and folder

--- prisma/prisma.service.ts
+++ prisma/prisma.service.ts
@@ -6,7 +6,6 @@ export class PrismaService extends PrismaClient implements OnModuleInit, OnModul
   async onModuleInit() {
     await this.$connect();
   }
-
   async onModuleDestroy() {
     await this.$disconnect();
   }
