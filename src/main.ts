4f7913de9e537252c35abe00d61bb6699f72580c
Eduardo <xiltepin@gmail.com>
2024-10-11 11:42:43 +0900

    added cross requests

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['log', 'error', 'warn', 'debug', 'verbose']
   });
+  // Enable CORS
+  app.enableCors({
+    origin: 'http://localhost:4200', // Replace this with the origin of your frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   await app.listen(3000);
   console.log("app bootstrapped");
 }
